1693d50cbf6bcf8b93fea7100dbedd8268ea2d84
HaleyOtaku <haleyotaku@gmail.com>
2024-06-11 11:13:35 -0500

    Completion of project commit.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,46 @@
-//Following this tutorial -> https://youtu.be/waCkrUIlSTo?si=5xzVA8juhhSuWOwa
+import Navbar from "./Components/Navbar"
+import Newsboard from "./Components/Newsboard"
+import { useState } from "react";
 
+//Following this tutorial -> https://youtu.be/waCkrUIlSTo?si=5xzVA8juhhSuWOwa
 const App = () => {
+  //State Hooks
+  const [category, setCategory] = useState("general");
+
   return (
     <div>
 
+      {/* All Bootstrap Buttons
+      <button type="button" className="btn btn-primary">Primary</button>
+      <button type="button" className="btn btn-secondary">Secondary</button>
+      <button type="button" className="btn btn-success">Success</button>
+      <button type="button" className="btn btn-danger">Danger</button>
+      <button type="button" className="btn btn-warning">Warning</button>
+      <button type="button" className="btn btn-info">Info</button>
+      <button type="button" className="btn btn-light">Light</button>
+      <button type="button" className="btn btn-dark">Dark</button>
+      <button type="button" className="btn btn-link">Link</button>
+
+        Bootstrap Dropdown Menu
+        <div className="dropdown">
+            <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Dropdown button
+            </button>
+            <ul className="dropdown-menu">
+              <li><a className="dropdown-item" href="#">Action</a></li>
+              <li><a className="dropdown-item" href="#">Another action</a></li>
+              <li><a className="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+        </div>
+
+        Bootstrap Spinner
+        <div className="spinner-border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </div> 
+        */}
+
+        <Navbar setCategory={setCategory}/>
+        <Newsboard category={category}/>
     </div>
   )
 }
---
+++ src/Components/Newsboard.jsx
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+import NewsItem from "./NewsItem";
+
+const Newsboard = ({category}) => {
+  // Hooks
+  const [articles, setArticles] = useState([]);//initialize with empty array
+
+
+  useEffect(()=>{
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`;
+    fetch(url).then(response=> response.json()).then(data=> setArticles(data.articles));
+  },[category])
+
+  return (
+    <div>
+      <h2 className="text-center">Latest <span className="badge bg-danger">News</span></h2>
+      {articles.map((news,index)=>{
+        return <NewsItem key={index} title={news.title} description={news.description} src={news.urlToImage} url={news.url}/>
+      })}
+    </div>
+  )
+}
+export default Newsboard
